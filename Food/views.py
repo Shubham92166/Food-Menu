47311e1d1683ad6fa65d17a2bdfd33b5024279e9
Shubham Singh <shubham92166@gmail.com>
2022-09-23 23:35:05 +0530

    Added changes

--- Food/views.py
+++ Food/views.py
@@ -1,8 +1,23 @@
+from multiprocessing import context
+from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import item
+from django.template import loader
 # Create your views here.
 def index(request):
     allItems = item.objects.all()
-    print(allItems)
-    return HttpResponse(allItems)
+    
+    context = {
+        'allItems' : allItems
+     }
+    return render(request, "Food/index.html", context)
+
+def detail(request, item_id):
+    item_detail = item.objects.get(pk = item_id)
+    context = {
+        'item_detail' : item_detail,
+    }
+    return render(request, "Food/detail.html", context)
+
+   
