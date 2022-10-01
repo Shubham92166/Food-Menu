bf4d1868524d37736a324c9088e719dae9d1a3ba
Shubham Singh <shubham92166@gmail.com>
2022-10-02 01:05:33 +0530

    Added images and styling with CSS and bootstrap

--- Food/models.py
+++ Food/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
@@ -6,3 +8,5 @@ class item(models.Model):
     item_name = models.CharField(max_length = 200)
     item_desc = models.CharField(max_length = 200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length = 200, default = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJrurcz8RuuhpJ3MAUaFGANA-8CClm7YPKfSiC6Sgzhs81NM2sS4da40mAX74yet8rIQk&usqp=CAU")
+    
