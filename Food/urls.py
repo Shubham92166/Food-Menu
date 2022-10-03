88e53aa39efa4766745696c572519fb97d9c34e1
Shubham Singh <shubham92166@gmail.com>
2022-10-03 19:29:49 +0530

    Added add_item functionality

--- Food/urls.py
+++ Food/urls.py
@@ -6,4 +6,5 @@ app_name = "food"
 urlpatterns = [
     path('', views.index, name = "index"), 
     path("<int:item_id>/", views.detail, name = "detail"),
+    path("add/", views.add_item, name = "add_item"),
 ]
